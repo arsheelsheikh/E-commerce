260e075eedd62d3846f6e3fb69691159c47567d9
arsheelsheikh <sheikharsheel@gmail.com>
2023-10-18 19:57:37 +0530

    Navbar

--- src/pages/Order/Order.jsx
+++ src/pages/Order/Order.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import myContext from '../../context/data/myContext'
 import Layout from '../../components/layout/Layout'
 
 
 const Order = () => {
+  const context = useContext(myContext);
+  const {name, rollno} = context;
   return (
-    <Layout>Order</Layout>
+    <Layout>
+      Order
+      <h1>Name: {name}</h1>
+      <h1>Roll No. : {rollno}</h1>
+    </Layout>
   )
 }
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Layout from '../../components/layout/Layout'
+import myContext from '../../context/data/myContext';
 
 const Home = () => {
   return (
-    <Layout>Home</Layout>
+    <Layout>
+      Home
+    </Layout>
   )
 }
 
