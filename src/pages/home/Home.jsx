9b02249c5d49e3cb7d0ba37895302b2ed3cf71e9
arsheelsheikh <sheikharsheel@gmail.com>
2023-10-20 07:32:26 +0530

    hero section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,12 @@
 import React, { useContext } from 'react'
 import Layout from '../../components/layout/Layout'
 import myContext from '../../context/data/myContext';
+import HeroSection from '../../components/heroSection/HeroSection';
 
 const Home = () => {
   return (
     <Layout>
-      Home
+      <HeroSection/ >
     </Layout>
   )
 }
