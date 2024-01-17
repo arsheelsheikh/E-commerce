8157252f616e34fb26448012658b7c48dcf0b007
arsheelsheikh <sheikharsheel@gmail.com>
2024-01-17 14:11:45 +0530

    Signup Authentication & Login And Protected Route

--- src/context/data/myState.jsx
+++ src/context/data/myState.jsx
@@ -15,8 +15,10 @@ function myState(props) {
       }
   }
 
+  const [loading, setLoading] = useState(false)
+
   return (
-    <MyContext.Provider value={{mode, toggleMode}}>
+    <MyContext.Provider value={{mode, toggleMode, loading, setLoading}}>
         {props.children}
     </MyContext.Provider>
   )
