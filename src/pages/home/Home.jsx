e5d4936ac863a75ea9eb0b96bb37b454cb73723e
arsheelsheikh <sheikharsheel@gmail.com>
2024-01-17 17:27:01 +0530

    it's done?

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,35 +6,22 @@ import Filter from '../../components/filter/Filter'
 import ProductCard from '../../components/productCard/ProductCard'
 import Track from '../../components/track/Track'
 import Testimonial from '../../components/testimonial/Testimonial'
-import { useDispatch, useSelector } from 'react-redux'
-import { addToCart, deleteFromCart } from '../../redux/cartSlice'
+import { Link } from 'react-router-dom'
 
 
 function Home() {
-  const dispatch = useDispatch();
-  const cartItem = useSelector((state)=> state.cart)
-
-  console.log(cartItem)
-
-  const addCart = () => {
-    dispatch(addToCart("shirt"));
-  }
-
-  const deleteCart = () => {
-    dispatch(deleteFromCart("shirt"));
-  }
   return (
     <Layout>
-      <div className="flex gap-5 justify-center">
-        <button className=' bg-gray-300 p-5' onClick={()=> addCart()}>add</button>
-        <button className=' bg-gray-300 p-5' onClick={()=> deleteCart()}>del</button>
-      </div>
       <HeroSection />
       <Filter />
       <ProductCard />
+      <div className="flex justify-center -mt-10 mb-4">
+        <Link to={'/allproducts'}>
+          <button className=' bg-gray-300 px-5 py-2 rounded-xl'>See more</button>
+        </Link>
+      </div>
       <Track />
       <Testimonial />
-
     </Layout>
   )
 }
